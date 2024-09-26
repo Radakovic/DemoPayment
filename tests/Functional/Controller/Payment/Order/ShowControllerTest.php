cf0f97d4c863d79a7c55d51eb9bd3b4e3c455b5f
rade <radakovicradoslav@gmail.com>
2024-09-26 18:37:05 +0200

    Controller tests

---
+++ tests/Functional/Controller/Payment/Order/ShowControllerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Tests\Functional\Controller\Payment\Order;
+
+use App\Entity\MerchantOrder;
+use App\Faker\Factory\MerchantOrderFactory;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ShowControllerTest extends WebTestCase
+{
+    private MerchantOrderFactory $orderFactory;
+    private EntityManagerInterface $entityManager;
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+
+        $this->orderFactory = self::getContainer()->get(MerchantOrderFactory::class);
+        $this->entityManager = self::getContainer()
+            ->get('doctrine')
+            ->getManager();
+    }
+
+    public function testShowForm(): void
+    {
+        $order = $this->createOrder();
+        $url = sprintf('/orders/invoices/%s', $order->getId()->toString());
+        $crawler = $this->client->request('GET', $url);
+        self::assertResponseIsSuccessful();
+
+        $h1 = $crawler->filter('h1')->text();
+        $amount = $crawler->filter("input[name=merchant_order\[amount\]]")->attr('value');
+
+        self::assertEquals($amount, ($order->getAmount() / 100));
+        self::assertEquals('Create invoice', $h1);
+    }
+
+    private function createOrder(): MerchantOrder
+    {
+        $order = ($this->orderFactory)();
+        $this->entityManager->persist($order);
+        $this->entityManager->flush();
+
+        return $order;
+    }
+}
