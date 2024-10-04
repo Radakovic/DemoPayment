840a235a13a43a5938c838c75c790f3d8da34a9a
rade <radakovicradoslav@gmail.com>
2024-10-04 13:23:34 +0200

    cache file system

--- src/Controller/Payment/Order/ShowController.php
+++ src/Controller/Payment/Order/ShowController.php
@@ -6,13 +6,25 @@ use App\Entity\Invoice;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class ShowController extends AbstractController
 {
+    public function __construct(private readonly CacheInterface $cache)
+    {
+    }
+
     #[Route('/invoices/{id}', name: 'show_invoice', methods: ['GET'])]
     public function __invoke(Invoice $invoice): Response
     {
-        $invoiceData = json_decode($invoice->getResponse(), true);
+        $id = $invoice->getId();
+        $invoiceData = $this->cache->get("invoice_data_$id", function (ItemInterface $item) use ($invoice) {
+
+            $item->expiresAfter(3600);
+            return json_decode($invoice->getResponse(), true);
+        });
+
         return $this->render(
             'invoice/show.html.twig',
             [
